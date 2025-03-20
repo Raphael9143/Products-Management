217120a8ec9494dbc357069412e578347df62330
Raphael9143 <s3jack3s@gmail.com>
2025-03-20 23:55:28 +0700

    [style] edit items

--- src/components/CardContainer.jsx
+++ src/components/CardContainer.jsx
@@ -3,12 +3,14 @@ import ProductCard from "./Card/ProductCard";
 import { FormatPrice } from "../utils/FormatPrice";
 import './CardContainer.css'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faCircleXmark } from "@fortawesome/free-solid-svg-icons";
+import { faCircleXmark, faL, faPenToSquare, faSave } from "@fortawesome/free-solid-svg-icons";
 
 export default function CardContainer({data}) {
     const items = data.customAttributes.productlist.items
     const [isOpen, setIsOpen] = useState(false)
     const [selected, setSelected] = useState(null)
+    const [isEdit, setIsEdit] = useState(false)
+    const [editedProduct, setEditedProduct] = useState({});
 
     const handleClick = (id) => {
         const chosenProduct = items[id] 
@@ -18,6 +20,19 @@ export default function CardContainer({data}) {
 
     const handleClose = () => {
         setIsOpen(false)
+        setIsEdit(false)
+    }
+
+    const handleEdit = () => {
+        setIsEdit(true)
+    }
+
+    const handleChange = () => {
+
+    }
+
+    const handleSave = () => {
+        setIsEdit(false)
     }
     
     return (
@@ -41,10 +56,40 @@ export default function CardContainer({data}) {
                             <img src={selected.imageSrc} alt={selected.name}/>
                         </div>
                         <div className='popup-content'>
-                            <h2>{selected.name}</h2>
-                            <p>Price: {FormatPrice(selected.price)} đ</p>
-                            <p>Details: lorem</p>
+                            {isEdit ? (
+                                <div className={`content ${isEdit ? 'open' : ''}`}>
+                                    <input 
+                                        type="text" 
+                                        name="name" 
+                                        value={editedProduct.name}
+                                        maxLength={20} 
+                                        placeholder='Tên sản phẩm'
+                                        onChange={handleChange} 
+                                    />
+                                    <input 
+                                        type="number" 
+                                        name="price" 
+                                        value={editedProduct.price}
+                                        max={100000000}
+                                        min={10000} 
+                                        placeholder="Giá"
+                                        onChange={handleChange} 
+                                    />
+                                </div>
+                            ) : (
+                                <div className='content'>
+                                    <h2>{selected.name}</h2>
+                                    <p>Price: {FormatPrice(selected.price)} đ</p>
+                                </div>
+                            )}
+                            <p>Details: {selected.details || 'lorem'}</p>
                         </div>
+                        {isEdit ? 
+                        (
+                            <FontAwesomeIcon icon={faSave} onClick={handleSave} className="edit"/>
+                        ) : (
+                            <FontAwesomeIcon icon={faPenToSquare} onClick={handleEdit} className="edit"/>
+                        )}
                         <FontAwesomeIcon icon={faCircleXmark} onClick={handleClose} className="close"/>
                     </div>
                 </div>
