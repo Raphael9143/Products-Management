f47cabb24e4a846d2ba12bd718cdbe519e2a8372
Đỗ Đình Dũng <s3jack3s@gmail.com>
2025-03-22 00:42:55 +0700

    [update] App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import {
 } from "react-router-dom";
 import HomeLayout from "./layouts/HomeLayout";
 import Home from "./pages/Home";
-import ProductsDataProvider from "./components/Context/ProductsContext";
+import ProductsDataProvider from "./components/context/ProductsContext";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
